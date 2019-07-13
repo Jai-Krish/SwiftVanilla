620c07522404f490959da2af82129bc100be3059
Jai Krishnan <mailtojaikrish@gmail.com>
2019-07-12 22:54:15 -0700

    Initial Commit

---
+++ VanillaSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VanillaSample
+//
+//  Created by JK on 12/07/19.
+//  Copyright © 2019 Jai Krishnan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
